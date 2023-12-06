5965162ec4007d5addec9924891b57a5e13036cc
ryancnelson <ryan@ryan.net>
2023-12-06 15:15:16 -0500

    update the start-alpine.sh script so it can be self-bootstrapping

--- start-alpine.sh
+++ start-alpine.sh
@@ -1,5 +1,36 @@
-#!/bin/sh 
+#!/usr/bin/bash
+# this FILE is public domain, by @ryancnelson on github, 2023
+# to be used with a tarball ryan is currently providing at 
+# licenses on stuff in that tarball are still their own, from the original source
+
+## bail out if error:
+set -e
+
+
+mychrootdir=/storage/ryanstuff/alpine-chroot
+
+mkdir -p $mychrootdir
+cd $mychrootdir
+
+
+# fetch the 'xpkg' aarch64 binary that we'd expect to run on your powkiddy x55
+curl -O pkgx https://github.com/pkgxdev/pkgx/releases/download/v1.1.1/pkgx-1.1.1+linux+aarch64.tar.xz.asc
+chmod +x pkgx
 
 
 
-chroot /storage/ryanstuff/alpine-chroot /bin/bash -l
+mount -o bind /dev /storage/${mychrootdir}/dev
+mount -o bind /proc /storage/${mychrootdir}/proc
+mount -o bind /sys /storage/${mychrootdir}/sys
+
+cp -L /etc/resolv.conf /storage/${mychrootdir}/etc/resolv.conf
+
+
+## i have hardcoded "http://mirror.clarkson.edu/alpine/latest-stable/main" in 
+# /etc/apk/repositories , as this script is intended to work with a tarball of my 
+# own creation, which is where this bit is hardcoded.
+# So, do nothing re: apk here
+
+
+
+
